1aef59382ae21eafaa4cae3b55484be7fdede99d
Doug Bellinger <doug.bellinger@gmail.com>
2024-09-17 15:16:02 -0400

    Made API_Config.py runnable.   Changed __dict__ ref to __dataclass__fields__

--- api/API_Config.py
+++ api/API_Config.py
@@ -1,28 +1,41 @@
 import os
 from dotenv import load_dotenv, find_dotenv, set_key
 from dataclasses import dataclass
-
+class password(str):
+    def __str__(self):
+        return '*' * len(self)
+    
 @dataclass(frozen=False)
-class API_Config:
+class API_Config():
     MPA_HOST:str|None = os.getenv('MPA_HOST') 
     MPA_UID:str|None = os.getenv('MPA_UID') 
-    MPA_PW:str|None = os.getenv('MPA_PW') 
+    MPA_PW:password|None = password(os.getenv('MPA_PW'))
     MPA_CONTAINER:str|None = os.getenv("MPA_CONTAINER") 
     SNOW_HOST:str|None = os.getenv('SNOW_HOST') 
     SNOW_UID:str|None = os.getenv('SNOW_UID') 
-    SNOW_PW:str|None = os.getenv('SNOW_PW') 
+    SNOW_PW:password|None = password(os.getenv('SNOW_PW'))
     def __init__(self):
         self.path = find_dotenv()
         load_dotenv(self.path)
 
     def save(self):
-        for key,value in self.__dict__.items():
-            if (key=='path'): 
-                continue
-            set_key(self.path, key_to_set=key, value_to_set=value)
-        print("Saved")
+        for field in API_Config.__dataclass_fields__:
+            value = getattr(API_Config, field)
+            set_key(self.path, key_to_set=field, value_to_set=value)
+        print(f"Saved to {self.path}")
+        
+    def prompt(self):
+        for field in API_Config.__dataclass_fields__:
+            while True:
+                value = getattr(API_Config, field)
+                setattr(API_Config, field, input(f"{field} ({value}):") or value)
+                if (getattr(API_Config, field)) :
+                    break    
+        
+            
 
 if __name__ == "__main__":
-    import pprint
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(API_Config.__dict__)
+    a = API_Config()
+    a.prompt()
+    a.save()
+
